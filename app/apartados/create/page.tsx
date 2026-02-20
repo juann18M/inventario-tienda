a1a019fe0c72aff4bc59dde187c31723bfe77b3c
juann18M <juanpablo13medi@gmail.com>
2026-02-20 16:10:42 -0600

    create apartado page

---
+++ app/apartados/create/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function CreateApartadoPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Crear Apartado</h1>
+      <p className="text-gray-500 mt-2">
+        Aquí podrás registrar un nuevo apartado.
+      </p>
+    </div>
+  );
+}
